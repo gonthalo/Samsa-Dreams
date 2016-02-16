a401b3b6a8f80cbc38349a25196309c86b9464d9
Gonthalo <gonzalogalpmg@gmail.com>
2016-02-16 20:21:00 +0100

    Update juego.py

--- juego.py
+++ juego.py
@@ -8,7 +8,10 @@ screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 pygame.display.set_caption('Juego Chachidiver')
 vel_anim = 10
+key_controls = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
 bucle = True
+exit_game_forever = False
+dimensiones_laberinto = (25, 25)
 
 camino = inspect.getfile(inspect.currentframe())
 print("Fichero:", camino)
@@ -16,8 +19,8 @@ print("Camino:", camino[:-8])
 
 img_h = 64
 img_w = 80
-bicho_1 = pygame.image.load(camino[:-8] + "bicho_2.png")
-rectangulo = bicho_1.get_rect().move(-img_w/2, -img_h/2)
+img_1 = pygame.image.load(camino[:-8] + "bicho_2.png")
+rectangulo = img_1.get_rect().move(-img_w/2, -img_h/2)
 
 def rand():
 	return random()*2 - 1
@@ -34,7 +37,25 @@ def mezclar(color1, color2):
 	f = media*1.0/media2
 	return tuple(colorm)#tuple([int(f*colorm[ii] + 0.5) for ii in range(3)])
 
-print("Making color images",)
+def poner_texto(cadena, alto):
+	screen.fill((0, 0, 0))
+	pygame.font.init()
+	fuente = pygame.font.Font(None, 50)
+	dy = alto*len(cadena)/2
+	for ind in range(len(cadena)):
+		texto = fuente.render(cadena[ind], True, (255, 255, 255), (0, 0, 0))
+		dx = texto.get_rect().width/2
+		screen.blit(texto, texto.get_rect().move(screen_width/2 - dx, screen_height/2 + alto*ind - dy))
+	pygame.display.flip()
+
+poner_texto(["Un jueguecito", "de la mano de", "ANGOSOFT"], 50)
+
+pygame.mixer.init()
+print('Volumen:', pygame.mixer.music.get_volume())
+pygame.mixer.music.load(camino[:-8] + "Introduccion.mp3")
+pygame.mixer.music.play()
+
+print "Haciendo bichitos de colores:",
 time0 = time()
 
 bichos_de_colores = []
@@ -54,14 +75,14 @@ for ii in range(4):
 					cccc.r, cccc.g, cccc.b = tttt
 					bichos_de_colores[ii][jj][kk].set_at((xx, yy), cccc)
 
-print(int((time() - time0)*100)/100.0, 'segundos')
+print int((time() - time0)*100)/100.0, 'segundos' 
 
 class Imagen:
 	def __init__(self, file_name):
 		self.area = pygame.image.load(camino[:-8] + file_name)
 		self.rect = self.area.get_rect()
-	def poner(self, i_x, i_y):
-		screen.blit(self.area, self.rect.move(i_x, i_y))
+	def poner(self, i_x, i_y, phi=0):
+		screen.blit(pygame.transform.rotate(self.area, phi), self.rect.move(i_x, i_y))
 
 class Bicho:
 	iden = -1
@@ -82,9 +103,9 @@ class Bicho:
 		self.color = tuple([int(random()*4)*85 for ii in range(3)])
 		self.radio = random()*20
 	def dibujar(self):
-		screen.blit(pygame.transform.rotate(bichos_de_colores[self.color[0]/85][self.color[1]/85][self.color[2]/85], self.phi/math.pi*180), rectangulo.move(self.x, self.y))
+		screen.blit(pygame.transform.rotate(bichos_de_colores[self.color[0]/85][self.color[1]/85][self.color[2]/85], self.phi/math.pi*180),rectangulo.move(self.x, self.y))
 		#pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), int(self.radio) + 1, 1)
-	def mover(self, ran):
+	def mover(self):
 		self.x += self.vx
 		self.y += self.vy
 		self.phi = math.atan(-self.vy/self.vx)
@@ -101,24 +122,27 @@ class Bicho:
 
 def delta(r):
 	r = r%4
-	aaa = (r == 0) - (r == 2)
-	bbb = (r == 1) - (r == 3)
+	aaa = (r == 3) - (r == 2)
+	bbb = (r == 1) - (r == 0)
 	return aaa, bbb
 
 class Laberinto:
 	size = (12, 12)
 	start = (5, 5)
 	matris = [[True for ii in range(size[0])] for jj in range(size[1])]
-	def __init__(self):
-		self.size = (14, 14)
+	def __init__(self, file_name, file_name2, size):
+		self.size = size
 		self.start = (int(self.size[0]/2), int(self.size[1]/2))
-		self.wall_file = 'bricks.png'
+		self.wall_file = file_name
+		self.ground_file = file_name2
+		self.pared = Imagen(self.wall_file)
+		self.suelo = Imagen(self.ground_file)
 		self.matris = [[True for ii in range(self.size[0])] for jj in range(self.size[1])]
 	def make_exit(self, point, last):
 		xx, yy = point
 		if xx in [self.size[0] - 1, 0] or yy in [self.size[1] - 1, 0]:
 			self.matris[xx][yy] = False
-			return True
+			return [point]
 		ccc = 0
 		for ii in range(4):
 			aa, bb = delta(ii)
@@ -131,14 +155,15 @@ class Laberinto:
 		for ii in range(4):
 			if (r + ii)%4 != last%4:
 				aa, bb = delta(r + ii)
-				if self.make_exit((xx + aa, yy + bb), r + ii + 2):
-					self.hacer_camino(point, False)
-					return True
+				result = self.make_exit((xx + aa, yy + bb), r + ii + 2)
+				if result != False:
+					#self.hacer_camino(point, False)
+					return [point] + result
 		self.matris[xx][yy] = True
 		return False
-	def hacer_camino(self, point, sub):
+	def hacer_camino(self, point, sub, depth):
 		xx, yy = point
-		if xx in [self.size[0] - 1, 0] or yy in [self.size[1] - 1, 0]:
+		if xx in [self.size[0] - 1, 0] or yy in [self.size[1] - 1, 0] or depth == 0:
 			return None
 		if sub:
 			ccc = 0
@@ -153,32 +178,34 @@ class Laberinto:
 		for ii in range(4):
 			aa, bb = delta(r + ii)
 			if self.matris[xx + aa][yy + bb]:
-				self.hacer_camino((xx + aa, yy + bb), True)
+				self.hacer_camino((xx + aa, yy + bb), True, depth - 1)
 	def generar(self):
-		self.make_exit(self.start, 4)
-		self.hacer_camino(self.start, False)
-	def dibujar(self):
-		pared = Imagen(self.wall_file)
+		#self.make_exit(self.start, 4)
+		#self.hacer_camino(self.start, False)
+		lista_de_puntos = self.make_exit(self.start, 4)
+		for punto in lista_de_puntos:
+			self.hacer_camino(punto, False, int(self.size[0] + self.size[1]))
+	def dibujar(self, center):
+		dx = self.pared.rect.width
+		dy = self.pared.rect.height
 		for ii in range(self.size[0]):
 			for jj in range(self.size[1]):
 				if self.matris[ii][jj]:
-					pared.poner(50*ii, 50*jj)
+					self.pared.poner(screen_width/2 + dx*(ii - center[0] - 0.5), screen_height/2 + dy*(jj - center[1] - 0.5))
+				else:
+					self.suelo.poner(screen_width/2 + dx*(ii - center[0] - 0.5), screen_height/2 + dy*(jj - center[1] - 0.5))
 		#bieeeeeen dibujos
 
 
 pygame.display.set_icon(pygame.image.load(camino[:-8] + "bicho_icono.png"))
 bichitos = []
 index = 0
-pygame.mixer.init()
-print('Volumen:', pygame.mixer.music.get_volume())
 
-pygame.mixer.music.load(camino[:-8] + "Chopin.mp3")
-pygame.mixer.music.play()
-
-mylab = Laberinto()
+"""
+screen.fill((255, 255, 255))
+mylab = Laberinto("bricks.png", (14, 14))
 mylab.generar()
-mylab.dibujar()
-
+mylab.dibujar(mylab.start)
 pygame.display.flip()
 
 for iii in range(1000):
@@ -187,20 +214,74 @@ for iii in range(1000):
 			bucle = False
 	clock.tick(200)
 
-while bucle:
+pygame.font.init()
+fuente = pygame.font.Font(None, 50)
+cadena = ['Una ma\xf1ana, tras un sue\xf1o intranquilo, ', 'Gregorio Samsa se despert\xf3 convertido en un monstruoso insecto.']
+for ind in range(2):
+	texto = fuente.render(cadena[ind], True, (255, 255, 255), (0, 0, 0))
+	screen.blit(texto, texto.get_rect().move(0, 50*ind))
+
+pygame.display.flip()
+"""
+for iii in range(1000):
 	for evento in pygame.event.get():
 		if evento.type == pygame.QUIT:
-			bucle = False
+			exit_game_forever = True
+	clock.tick(200)
+
+#Samsa esta en el laberinto:
+pygame.mixer.music.stop()
+pygame.mixer.music.load(camino[:-8] + "Laberinto.mp3")
+pygame.mixer.music.play(-1)
+
+samsa_img = Imagen("bicho_3.png")
+maze = Laberinto("bricks_75.png", "ground_75.png", dimensiones_laberinto)
+maze.generar()
+screen.fill((255, 255, 255))
+samsa_x, samsa_y = maze.start
+longitud = maze.pared.rect.width
+maze.dibujar((samsa_x, samsa_y))
+samsa_width, samsa_height = samsa_img.rect.width, samsa_img.rect.height
+samsa_img.poner(screen_width/2 - samsa_width/2, screen_height/2 - samsa_height/2)
+
+while bucle and not exit_game_forever:
+	for evento in pygame.event.get():
+		if evento.type == pygame.QUIT:
+			exit_game_forever = True
+	clock.tick(vel_anim)
+	teclas = [pygame.key.get_pressed()[ii]!=0 for ii in key_controls]
+	if sum(teclas)!=0:
+		dx, dy = delta(teclas.index(True))
+		if not maze.matris[samsa_x + dx][samsa_y + dy]:
+			samsa_x += dx
+			samsa_y += dy
+			screen.fill((255, 255, 255))
+			maze.dibujar((samsa_x, samsa_y))
+			samsa_img.poner(screen_width/2 - samsa_width/2, screen_height/2 - samsa_height/2, phi = [90, 270, 180, 0][teclas.index(True)])
+		#samsa_img.poner(screen_width/2 - samsa_width/2, screen_height/2 - samsa_height/2, phi = [90, 270, 180, 0][teclas.index(True)])
+	pygame.display.flip()
+	if samsa_y == 0 or samsa_y == maze.size[1] - 1 or samsa_x == 0 or samsa_x == maze.size[0] - 1:
+		bucle = False
+
+#bichitos de colores moviendose por toda la pantalla:
+bucle = True
+
+pygame.mixer.music.stop()
+pygame.mixer.music.load(camino[:-8] + "Chopin.mp3")
+pygame.mixer.music.play()
+
+while bucle and not exit_game_forever:
+	for evento in pygame.event.get():
+		if evento.type == pygame.QUIT:
+			exit_game_forever = True
 
 	#pygame.draw.circle(screen, (200, 200, 200), (index, 200), 20, 2)height
 	clock.tick(vel_anim)
 	screen.fill((255, 255, 255))
-	#print 'bucle', index
-	#mierda(mierda, mas_mierda)
 	buggy = Bicho(index)
 	bichitos.append(buggy)
 	for bichito in bichitos:
-		bichito.mover(math.sqrt(index)/2)
+		bichito.mover()
 		bichito.dibujar()
 	index += 1
 	#blablabla()
